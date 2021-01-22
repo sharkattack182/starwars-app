1b3aeefc5d1d5c9612f693f7194a06f2af427804
Matthew Courtney <matthewdc182@gmail.com>
2021-01-21 20:03:33 -0500

    used req.params to redduce the amount of code needed also set the characters into an array to display all of them or loop through and chose a specific one

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 var express = require("express");
 const { get } = require("http");
+const { send } = require("process");
 
 var app = express();
 var PORT = 8080;
 
 // Data
-var yoda = {
+var characters = [
+    {
+    routeName: "yoda",
     name: "Yoda",
     role: "Jedi Master",
     age: 900,
     forcePoints: 2000
-};
-
-var darthVader = {
+},
+{
+    routeName: "darthvader",
     name: "Darth Vader",
     role: "Sith Lord",
     age: 22,
     forcePoints: 3000
-};
-
-var obi = {
+},
+{
+    routeName: "obiwan",
     name: "Obi Wan Kenobi",
     role: "Jedi Master",
     age: 38,
     focePoints: 1500
-};
+}]
 
-
+console.log(characters.length)
 // Routes
 app.get("/", function(req,res) {
     res.send("Welcome to the Star Wars app! May the force be with you.")
 });
-
-app.get("/yoda", function(req,res) {
-    res.send(yoda);
+app.get("/api/characters", function(req,res) {
+    return res.json(characters)
 })
 
-app.get("/darthvader", function(req,res) {
-    res.send(darthVader);
+app.get("/api/characters/:character", function(req,res) {
+    var chosen = req.params.character
+
+    console.log(chosen);
+    for(let i = 0; i < characters.length; i++) {
+        console.log(characters[i]);
+        if(chosen === characters[i].routeName) {
+            return res.json(characters[i])
+        }
+    }
 })
-
-app.get("/obiwan", function(req,res) {
-    res.send(obi)
-});
-
 
 app.listen(PORT, function() {
     console.log("Now listening on PORT" + PORT)
