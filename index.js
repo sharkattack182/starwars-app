990f1f625600880a18270594f5da990289bd34f7
Matthew Courtney <matthewdc182@gmail.com>
2021-01-23 20:29:18 -0500

    updated the server port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
